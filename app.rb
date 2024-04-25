0213939f67c7d4bd5fda245de7882e371b7cc44c
Shingie Taira <tairashingie@gmail.com>
2024-04-25 23:31:04 +0900

    got the base code working

--- app.rb
+++ app.rb
@@ -1,7 +1,40 @@
-require "sinatra"
-require "sinatra/reloader" if development?
-require "pry-byebug"
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'pry-byebug'
+require_relative 'cookbook'
+require_relative 'recipe'
+
+cookbook = Cookbook.new
 
 get "/" do
-  "Hello world!"
+  @recipes = cookbook.all || []
+  erb :index
+end
+
+get "/new" do
+  erb :new
+end
+
+post "/recipes" do
+  name = params[:name]
+  description = params[:description]
+  cookbook.create(Recipe.new(name, description))
+  redirect "/"
+end
+
+get "/delete/:index" do
+  index = params[:index].to_i
+  cookbook.destroy(index)
+  redirect "/"
+end
+
+get "/team/:username" do
+  puts params[:username]
+  "The username is #{params[:username]}"
+end
+
+
+
+get "/about" do
+  erb :about
 end
---
+++ cookbook.rb
@@ -0,0 +1,17 @@
+class Cookbook
+  def initialize
+    @recipes = []
+  end
+
+  def all
+    @recipes
+  end
+
+  def create(recipe)
+    @recipes << recipe
+  end
+
+  def destroy(recipe_index)
+    @recipes.delete_at(recipe_index)
+  end
+end
